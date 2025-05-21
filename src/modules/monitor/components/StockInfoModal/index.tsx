a169c88f0445f6be5729b8e1ef755d4de7c3e61a
Elena Chingaeva <e.chingaeva@spectr.dev>
2025-05-21 11:30:50 +0500

    График изменения стоимости акции

--- src/modules/monitor/components/StockInfoModal/index.tsx
+++ src/modules/monitor/components/StockInfoModal/index.tsx
@@ -1,8 +1,12 @@
+"use client";
+
 import { Modal } from "@/ui/Modal";
 import { FC } from "react";
 import { useTimeSeries } from "../../queries/useTimeSeries";
 import { LoadingPanel } from "@/ui/LoadingPanel";
-import { Box } from "@mui/material";
+import dynamic from "next/dynamic";
+
+const Chart = dynamic(() => import("react-apexcharts"), { ssr: false });
 
 interface Props {
   onClose: () => void;
@@ -10,18 +14,76 @@ interface Props {
 }
 
 export const StockInfoModal: FC<Props> = ({ onClose, symbol }) => {
-  const { isLoading } = useTimeSeries({ symbol });
+  const { data, isLoading } = useTimeSeries({ symbol });
+
+  const priceData = {
+    values: data?.values.map((item) => Number(item.close)) ?? [],
+    times: data?.values.map((item) => new Date(item.datetime).getTime()) ?? [],
+  };
+
+  const series = [
+    {
+      name: "Цена закрытия",
+      type: "area",
+      data: priceData.values,
+    },
+  ];
+
+  const options = {
+    chart: {
+      type: "area",
+      zoom: {
+        type: "x",
+        enabled: true,
+      },
+    },
+    dataLabels: {
+      enabled: false,
+    },
+    labels: priceData.times,
+    xaxis: {
+      type: "datetime",
+    },
+    yaxis: [
+      {
+        labels: {
+          formatter: function (val: number) {
+            return val;
+          },
+        },
+      },
+    ],
+    fill: {
+      type: "gradient",
+      gradient: {
+        shadeIntensity: 1,
+        inverseColors: false,
+        opacityFrom: 0.7,
+        opacityTo: 0,
+        stops: [0, 70, 100],
+      },
+    },
+  };
 
   return (
     <Modal
       title={`Стоимость акции ${symbol}`}
       onClose={onClose}
       open={!!symbol}
-      slotProps={{ paper: { sx: { minWidth: 500 } } }}
+      slotProps={{
+        paper: {
+          sx: {
+            minWidth: 500,
+            minHeight: 400,
+            position: "relative",
+            padding: 2,
+            overflow: "hidden",
+          },
+        },
+      }}
     >
-      <Box sx={{ position: "relative", minHeight: 400 }}>
-        {isLoading && <LoadingPanel />}
-      </Box>
+      {isLoading && <LoadingPanel />}
+      {data && <Chart type="area" options={options as any} series={series} />}
     </Modal>
   );
 };
